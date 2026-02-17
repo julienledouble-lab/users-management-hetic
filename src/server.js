338fc68c7fdd58bac2b2cd8599799194120bcb37
julienledouble-lab <julienledouble@gmail.com>
2026-02-17 15:25:11 +0100

    third

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@
 // console.log('Server running at http:/localhost:3000/');
 
 import express from 'express'
-
+import dotenv from 'dotenv';
 const app = express();
 
 app.use(express.json());
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 });
 });
 
-
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`)
 })
