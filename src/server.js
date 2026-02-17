e1f237de9807393f09b0136e3631741b6045e3f9
julienledouble-lab <julienledouble@gmail.com>
2026-02-17 14:40:24 +0100

    integrate express

--- src/server.js
+++ src/server.js
@@ -1,19 +1,36 @@
-import http from 'node:http';
-import {json} from './utils/responses.js';
-import {listUsers} from './users/user.controller.js';
+// import http from 'node:http';
+// import {json} from './utils/responses.js';
+// import {listUsers} from './users/user.controller.js';
 
-const listener = (request, response) => {
-    if (request.url === '/users') {
-        return listUsers(request, response);
-    }
+
+// const listener = (request, response) => {
+    // if (request.url === '/users') {
+        // return listUsers(request, response);
+    // }
     //get users est déclaré ici
     
-    return json (response, 404, {
-        message: 'Not found'
-    });
-};
+//     return json (response, 404, {
+//         message: 'Not found'
+//     });
+// };
 
-const server = http.createServer(listener);
-server.listen(3000);
 
-console.log('Server running at http:/localhost:3000/');
+// const server = http.createServer(listener);
+// server.listen(3000);
+
+// console.log('Server running at http:/localhost:3000/');
+
+import express from 'express'
+
+const app = (express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Users management API'
+});
+});
+    
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
--- src/utils/responses.js
+++
@@ -1,6 +0,0 @@
-export function json(res, status, data) {
-    res.writeHead(status, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(data));
-}
-/*chaque fois qu'on veut envoyer une réponse json, on peut utiliser cette fonction pour éviter de répéter le même code à chaque fois.*/ 
-
